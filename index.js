aa6bfb1c09250be5493d014e9a813732b3c6e502
annisaps313 <annisaps313@gmail.com>
2026-04-06 18:27:23 +0700

    Add function to get button value

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function multiply(a, b){
 }
 
 function division(a, b){
-	if b === 0 return "Error";
+	if (b === 0) return "Error";
 	return a / b;
 }
 
@@ -29,3 +29,47 @@ function operate(a, operator, b){
 			return null;
 	}
 }
+
+let firstNum = "";
+let secondNum = "";
+let operator;
+let isOperatorClicked;
+
+function getBtnValue(){
+	let display = document.querySelector(".display");
+	let btn = document.querySelectorAll("button");
+
+	btn.forEach((button) => {
+		button.addEventListener("click", () => {
+			let value = button.textContent;
+
+			if (value === "Clear"){
+				firstNum = "";
+				secondNum = "";
+				operator = null;
+				isOperatorClicked = false;
+				display.textContent = "";
+			} else if (value === "+" || value === "-" || value === "x" || value === "/"){
+				operator = value;
+				isOperatorClicked = true;
+			} else if (value === "="){
+				let result = operate(+firstNum, operator, +secondNum);
+				display.textContent = result;
+				firstNum = "";
+				secondNum = "";
+				operator = null;
+				isOperatorClicked = false;
+			} else {
+				if (!isOperatorClicked){
+					firstNum += value;
+					display.textContent = firstNum;
+				} else{
+					secondNum += value;
+					display.textContent = secondNum;
+				}
+			}
+		})
+	})
+}
+
+getBtnValue();
