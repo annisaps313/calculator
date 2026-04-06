5801287e7e265804ef66b16e757c4516d13b1f17
annisaps313 <annisaps313@gmail.com>
2026-04-06 17:35:34 +0700

    Add function operations

---
+++ index.js
@@ -0,0 +1,17 @@
+function add(a, b){
+	return a + b;
+}
+
+function subs(a, b){
+	return a - b;
+}
+
+function multiply(a, b){
+	return a * b;
+}
+
+function division(a, b){
+	if b === 0 return "Error";
+	return a / b;
+}
+
